0bda191b0610590b0dc6a6e6bdadbd948db06117
Mário Fernandes <mariobmf02@gmail.com>
2020-08-09 13:33:16 -0300

    Configured ESLint and Prettier

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from 'express';
 const app = express();
 
 app.get('/', (request, response) => {
-  return response.json({message: 'Initial Commit'});
-})
+  return response.json({ message: 'Initial Commit' });
+});
 
 app.listen(3333);
