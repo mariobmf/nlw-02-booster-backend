924dda7d0fea706ad3a5eade21e43a43369b5709
Mário Fernandes <mariobmf02@gmail.com>
2020-08-10 21:03:05 -0300

    Created routes for registration and listing of connections

---
+++ src/controllers/ConnectionsController.ts
@@ -0,0 +1,22 @@
+/* eslint-disable camelcase */
+import { Request, Response } from 'express';
+import db from '../database/connection';
+
+class ConnectionsController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const totalConnections = await db('connections').count('* as total');
+
+    const { total } = totalConnections[0];
+
+    return response.json({ total });
+  }
+
+  public async store(request: Request, response: Response): Promise<Response> {
+    const { user_id } = request.body;
+
+    await db('connections').insert({ user_id });
+
+    return response.status(201).send();
+  }
+}
+export default new ConnectionsController();
